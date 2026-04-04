a9b6a2f7092c7fcc6a4f2e9a17732acc113664b8
successfulceo23 <uzihsamuel@gmail.com>
2026-04-04 13:20:20 +0100

    mongoose url check fix

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { connectDB } from "./src/config/database";
+import app from "./src/app";
+
+const PORT = process.env.PORT || 3000
+
+connectDB().then(()=>{
+    app.listen(process.env.PORT, () => {
+        console.log(`server is running  http://localhost:${PORT}`);
+    })
+})
+.catch((error)=>{
+    console.error("failed to start server:",error);
+    process.exit(1);
+});
--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () =>{
     try {
+        const mongourl = process.env.MONGODB_URL;
+        if(!mongourl) {
+            throw new Error("MONGO_URL enviroment variable isn't defined");
+        }
         await mongoose.connect(Bun.env.MONGODB_URL as string);
         console.log("MongoDB connected successfully");
         
